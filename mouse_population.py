ae60e6c83b75c51b63741e253a641a8e3a286928
maxbossing <github@maxbossing.de>
2022-11-30 14:38:55 +0100

    renamed population.py to mouse_population.py

---
+++ mouse_population.py
@@ -0,0 +1,14 @@
+# 9.11.2022
+# Edit 16.11.2022
+# Calculates the poulation changes of a mouse colony for n years
+
+young,old,elderly = 6,9,12
+
+for i in range(0, int(input("iterations> "))):
+    print(f'|==========| {i + 1} |==========|')
+    
+    young, old, elderly = old * 4 + elderly * 2, young // 2, old // 3
+
+    print(f'{young}\n{old}\n{elderly}\nTotal = {young + old + elderly}')
+
+print(f'Loop finished, final Population : [Young = {young}, Old = {old}, Elderly = {elderly}, Total = {young + old + elderly}]')
